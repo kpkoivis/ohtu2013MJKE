eae576881628e5da63d97c4112a40b4f5868790a
Kristian Koivisto <kristiak@cs.helsinki.fi>
2013-10-08 22:38:11 +0300

    ViiteItem class

---
+++ src/main/java/wad/spring/domain/ViiteItem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.spring.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kris
+ */
+public class ViiteItem implements Serializable {
+
+    private String      fieldName;
+    private String      fieldValue;
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
